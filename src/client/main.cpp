ac2ed4205791770423096c800f724485b77df9ed
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2020-04-02 17:29:20 +0200

    add cli to server; move data into vector; bug with \0 still not fixed

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -7,60 +7,59 @@ using namespace std;
 using namespace asio::ip;
 
 int main() {
-    char a[280] = "Will be starting The White House news conference at 5:15 P.M. Eastern.";
-    char b[280] = "My proposal to the politically correct Automobile Companies would lower the average price of a car to consumers by more than $3500, while at the same time making the cars substantially safer. Engines would run smoother. Positive impact on the environment! Foolish executives!";
-    char c[280] = "My Administration is helping U.S. auto workers by replacing the failed Obama Emissions Rule. Impossible to satisfy its Green New Deal Standard; Lots of unnecessary and expensive penalties to car buyers!";
-
-    cleanup_char_arr(a);
-    cleanup_char_arr(b);
-    cleanup_char_arr(c);
-
-    vector<char*> v;
-    v.push_back(a);
-    v.push_back(b);
-    v.push_back(c);
+    vector<const char*> answers;
 
     // first element gets retrieved
     vector<int> idx;
-    idx.push_back(2);
     idx.push_back(0);
     idx.push_back(1);
+    idx.push_back(2);
 
+    vector<string> ports;
+    ports.push_back("1234");
+    ports.push_back("1235");
 
-    tcp::iostream strm{"localhost", "1234"};
-    if (strm) {
-        cout << "connection created" << endl;
+    bool first_server{true};
 
-        std::stringstream ss;
-        for(size_t i = 0; i < idx.size(); ++i)
-        {
-            if(i != 0) {
-                ss << ",";
+    for (string port : ports) {
+        tcp::iostream strm{"localhost", port};
+        if (strm) {
+            cout << "connection created" << endl;
+
+            std::stringstream ss;
+            size_t i{0};
+            if (first_server) {
+                i++;
+                first_server = false;
             }
-            ss << idx[i];
-        }
-        strm << ss.str() << endl;
-        string data;
-        getline(strm, data);
-        cout << "received data" << endl;
-        char* output;
-        bool first = true;
-        for (int i{1}; i < idx.size(); i++) {
-            if (first) {
-                output = xor_string(data.c_str(), v.at(idx.at(i)));
-                first = false;
-            } else {
-                output = xor_string(output, v.at(idx.at(i)));
+            for (; i < idx.size(); ++i) {
+                ss << idx[i];
+                if (i != idx.size() - 1) {
+                    ss << ",";
+                }
             }
+            cout << port << ": " << ss.str() << endl;
+            strm << ss.str() << endl;
+            string data;
+            //strm.read(data, 280);
+            getline(strm, data);
+            cout << "received data" << endl;
+            answers.push_back(data.c_str());
+            strm.close();
+            cout << "connection closed" << endl;
+        } else {
+            cout << "Could not connect to server!" << endl;
         }
-        cout << output << endl;
-        delete output;
-
-        strm.close();
-        cout << "connection closed" << endl;
-    } else {
-        cout << "Could not connect to server!" << endl;
     }
+
+    char* output;
+    output = xor_string(answers.at(0), answers.at(1));
+    for (int i{2}; i < answers.size(); i++) {
+        output = xor_string(output, answers.at(i));
+    }
+
+    cout << "output: " << output << endl;
+    delete output;
 
     return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,13 +2,21 @@
 #include <cstring>
 #include <asio.hpp>
 #include <vector>
+#include <CLI11.hpp>
 
 #include "bit_operations.h"
 
 using namespace std;
 using namespace asio::ip;
 
-int main() {
+int main(int argc, char* argv[]) {
+    CLI::App app{"Private Information Retrieval Server"};
+
+    unsigned short port;
+    app.add_option("-p,--port", port, "Port on which to send data on")->required(true);
+
+    CLI11_PARSE(app, argc, argv);
+
     char a[280] = "Will be starting The White House news conference at 5:15 P.M. Eastern.";
     char b[280] = "My proposal to the politically correct Automobile Companies would lower the average price of a car to consumers by more than $3500, while at the same time making the cars substantially safer. Engines would run smoother. Positive impact on the environment! Foolish executives!";
     char c[280] = "My Administration is helping U.S. auto workers by replacing the failed Obama Emissions Rule. Impossible to satisfy its Green New Deal Standard; Lots of unnecessary and expensive penalties to car buyers!";
@@ -22,10 +30,8 @@ int main() {
     v.push_back(b);
     v.push_back(c);
 
-    char* output;
-
     asio::io_context ctx;
-    tcp::endpoint ep{tcp::v4(), 1234};
+    tcp::endpoint ep{tcp::v4(), port};
     cout << "connection openend" << endl;
     tcp::acceptor acceptor{ctx, ep};
     cout << "listening" << endl;
@@ -38,37 +44,33 @@ int main() {
     if (strm) {
         string data;
         getline(strm, data);
-        cout << "got " << data << endl;
 
         size_t start = 0;
         size_t end = 0;
 
         end = data.find(',', start);
         int first = stoi(data.substr(start, end - start));
-        cout << first << endl;
-
         start = end + 1;
         end = data.find(',', start);
         int second = stoi(data.substr(start, end - start));
-        cout << second << endl;
 
-        output = xor_string(v.at(first), v.at(second));
+        char* output{xor_string(v.at(first), v.at(second))};
 
         while ((start = data.find_first_not_of(',', end)) != std::string::npos)
         {
             end = data.find(',', start);
             int message_idx = stoi(data.substr(start, end - start));
-            cout << message_idx << endl;
             output = xor_string(output, v.at(message_idx));
         }
         cout << "sending" << endl;
+        cout << output << endl;
         strm << output << endl;
+        delete output;
         strm.close();
     } else {
         cout << "could not open stream" << endl;
     }
     cout << "connection closed" << endl;
-    delete output;
 
     return 0;
 }
