954f75b439c412eba6719c110b1900250df113c2
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2020-04-03 12:17:58 +0200

    cleanup code

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <asio.hpp>
-#include <memory.h>
 
 #include "bit_operations.h"
 
@@ -11,18 +10,12 @@ int main() {
     vector<const char*> answers;
 
     // first element gets retrieved
-    vector<int> idx;
-    idx.push_back(0);
-    idx.push_back(1);
-    idx.push_back(2);
-
-    vector<string> ports;
-    ports.push_back("1234");
-    ports.push_back("1235");
+    vector<int> idx{2, 0, 1};
+    vector<string> ports{"1234", "1235"};
 
     bool first_server{true};
 
-    for (string port : ports) {
+    for (const string& port : ports) {
         tcp::iostream strm{"localhost", port};
         if (strm) {
             cout << "connection created" << endl;
@@ -39,7 +32,6 @@ int main() {
                     ss << ",";
                 }
             }
-            cout << port << ": " << ss.str() << endl;
             strm << ss.str() << endl;
             string data;
             char buffer[280];
@@ -53,7 +45,6 @@ int main() {
             char* answer = new char[280];
             memcpy(answer, data.c_str(), 280);
             answers.push_back(answer);
-            cout.write(data.c_str(), 280);
             strm.close();
             cout << "connection closed" << endl;
         } else {
@@ -64,12 +55,12 @@ int main() {
     char* output;
     output = xor_string(answers.at(0), answers.at(1));
     for (int i{2}; i < answers.size(); i++) {
-        cout << "Entering " << i << endl;
         output = xor_string(output, answers.at(i));
     }
 
     cout << "output: " << flush;
-    cout.write(output, 280);
+    cout.write(output, 279);
+    cout << endl;
     delete output;
 
     for (const char* a : answers) {
